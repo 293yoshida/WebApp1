d9983e4da619f1d0b5077db90612497305b3d83d
Hiroaki Yoshida <h.yoshida@newdaysys.co.jp>
2015-02-11 10:13:07 +0900

    仮定義関数の未実装警告用関数を追加

--- AWS_WEBApp1/WebContent/js/share/shareJS.js
+++ AWS_WEBApp1/WebContent/js/share/shareJS.js
@@ -6,7 +6,10 @@
  * グローバル関数
  * 先頭文字を大文字
  */
-
+ //ローカル関数の仮定義に必ず実装すること。ローカル関数の実装漏れ防止
+function UnimplementedAlert(var funcName){
+	alert("Unimplemented Function : " + funcName);
+}
 
 
 
@@ -29,8 +32,15 @@
  * 先頭文字を小文字
  */
 //onLoadで呼び出すための初期化用関数
-function init(){};
+function init(){UnimplementedAlert("init");};
 //ready関数で呼び出すための関数
-function readyFunction(){};
+function readyFunction(){UnimplementedAlert("readyFunction");};
 
-function headerDialog(){};
+//画面ＵＩのヘッダー部分ダイアログ
+function headerDialog(){UnimplementedAlert("headerDialog");};
+//画面ＵＩのメニュー部分ダイアログ
+function menuDialog(){UnimplementedAlert("menuDialog");};
+//画面ＵＩの設定部分ダイアログ
+function configDialog(){UnimplementedAlert("configDialog");};
+
+
